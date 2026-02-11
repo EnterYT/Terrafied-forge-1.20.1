4827884d1a28fbd2fdad7bbc6dbf4c0c17ba4937
User <toleu.era10.2004@gmail.com>
2026-02-12 02:19:34 +0500

    ThermalShockBlasterScreen progress arrow animation fix

--- src/main/java/net/enter/terrafied/screen/ThermalShockBlasterScreen.java
+++ src/main/java/net/enter/terrafied/screen/ThermalShockBlasterScreen.java
@@ -11,7 +11,6 @@ import net.minecraft.world.entity.player.Inventory;
 
 public class ThermalShockBlasterScreen extends AbstractContainerScreen<ThermalShockBlasterMenu> {
     private static final ResourceLocation TEXTURE = new ResourceLocation(Terrafied.MOD_ID, "textures/gui/thermal_shock_blaster_gui.png");
-
     public ThermalShockBlasterScreen(ThermalShockBlasterMenu pMenu, Inventory pPlayerInventory, Component pTitle) {
         super(pMenu, pPlayerInventory, pTitle);
     }
@@ -24,28 +23,28 @@ public class ThermalShockBlasterScreen extends AbstractContainerScreen<ThermalSh
     }
 
     @Override
-    protected void renderBg(GuiGraphics pGuiGraphics, float pPartialTick, int pMouseX, int pMouseY) {
+    protected void renderBg(GuiGraphics guiGraphics, float pPartialTick, int pMouseX, int pMouseY) {
         RenderSystem.setShader(GameRenderer::getPositionTexShader);
         RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
-        RenderSystem.setShaderTexture(0 ,TEXTURE);
+        RenderSystem.setShaderTexture(0, TEXTURE);
         int x = (width - imageWidth) / 2;
         int y = (height - imageHeight) / 2;
 
-        pGuiGraphics.blit(TEXTURE, x, y, 0, 0, imageWidth, imageHeight);
+        guiGraphics.blit(TEXTURE, x, y, 0, 0, imageWidth, imageHeight);
 
-        renderProgressionArrow(pGuiGraphics, x, y);
+        renderProgressArrow(guiGraphics, x, y);
     }
 
-    private void renderProgressionArrow(GuiGraphics pGuiGraphics, int x, int y) {
-        if (menu.isCrafting()) {
-            pGuiGraphics.blit(TEXTURE, x + 85, y + 30, 176, 0, 0, menu.getScaledProgress());
+    private void renderProgressArrow(GuiGraphics guiGraphics, int x, int y) {
+        if(menu.isCrafting()) {
+            guiGraphics.blit(TEXTURE, x + 85, y + 30, 176, 0, 8, menu.getScaledProgress());
         }
     }
 
     @Override
-    public void render(GuiGraphics pGuiGraphics, int pMouseX, int pMouseY, float pPartialTick) {
-        renderBackground(pGuiGraphics);
-        super.render(pGuiGraphics, pMouseX, pMouseY, pPartialTick);
-        renderTooltip(pGuiGraphics, pMouseX, pMouseY);
+    public void render(GuiGraphics guiGraphics, int mouseX, int mouseY, float delta) {
+        renderBackground(guiGraphics);
+        super.render(guiGraphics, mouseX, mouseY, delta);
+        renderTooltip(guiGraphics, mouseX, mouseY);
     }
 }
